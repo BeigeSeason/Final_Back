bef57555a71d5df847fa42f8990972662bcc30b9
BeigeSeason <kima606@gmail.com>
2025-02-21 09:26:35 +0900

    2025.02.21 강휘 스웨거

--- src/main/java/com/springboot/final_back/config/SwaggerConfiguration.java
+++ src/main/java/com/springboot/final_back/config/SwaggerConfiguration.java
@@ -16,6 +16,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -28,11 +29,11 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.SpringBoot.Plan4Land"))
+                .apis(RequestHandlerSelectors.basePackage("com.springboot.final_back"))
                 .paths(PathSelectors.any())
                 .build()
-                .securitySchemes(Arrays.asList(apiKey()))
-                .securityContexts(Arrays.asList(securityContext()));
+                .securitySchemes(List.of(apiKey()))
+                .securityContexts(Collections.singletonList(securityContext()));
     }
 
     private ApiInfo apiInfo() {
@@ -59,7 +60,7 @@ public class SwaggerConfiguration {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+        return List.of(new SecurityReference("JWT", authorizationScopes));
     }
 }
 
