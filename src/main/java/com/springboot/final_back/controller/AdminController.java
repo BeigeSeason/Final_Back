5ba72a2ac5fbe5101c3eef376f291025b58c1f31
MyNameIsYGKim <kelen536@gmail.com>
2025-02-24 17:58:33 +0900

    250224 연규(관리자-신고 수정

--- src/main/java/com/springboot/final_back/controller/AdminController.java
+++ src/main/java/com/springboot/final_back/controller/AdminController.java
@@ -12,6 +12,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 @RestController
 @RequestMapping("/admin")
@@ -32,10 +35,16 @@ public class AdminController {
 
     // 신고 조회
     @GetMapping("/report-list")
-    public ResponseEntity<Page<ReportResDto>> getReports(@RequestParam(defaultValue = "0") int page,
-                                                         @RequestParam(defaultValue = "10") int size,
-                                                         @RequestParam(required = false) String reportType) {
+    public ResponseEntity<Map<String, Object>> getReports(@RequestParam(defaultValue = "0") int page,
+                                                          @RequestParam(defaultValue = "10") int size,
+                                                          @RequestParam(required = false) String reportType) {
         Page<ReportResDto> reports = reportService.getReports(page, size, reportType);
-        return ResponseEntity.ok(reports);
+
+        // 응답에 필요한 데이터만 포함
+        Map<String, Object> response = new HashMap<>();
+        response.put("reports", reports.getContent());  // 요청된 페이지의 데이터
+        response.put("totalElements", reports.getTotalElements());  // 전체 데이터 수
+
+        return ResponseEntity.ok(response);
     }
 }
