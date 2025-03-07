e8cb04f443766eaa0148d4d2e442b830cdeb0317
BeigeSeason <kima606@gmail.com>
2025-03-07 15:49:00 +0900

    2025.03.07 강휘 리뷰Dto

--- src/main/java/com/springboot/final_back/dto/ReviewResDto.java
+++ src/main/java/com/springboot/final_back/dto/ReviewResDto.java
@@ -4,4 +4,10 @@ import lombok.Data;
 
 @Data
 public class ReviewResDto {
+    private Long id; // 해당 리뷰 고유 아이디(수정 시 사용)
+    private String memberId; // 리뷰 남긴 유저 아이디
+    private String nickname;
+    private String profileImg;
+    private float rating;  // 점수
+    private String content; // 내용
 }
